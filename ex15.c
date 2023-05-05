686599540e294fdb877857f07f6de2e5bdd406a0
Yasmin <souzasmimi@gmail.com>
2023-05-05 18:29:58 -0300

    13,14,15

---
+++ ex15.c
@@ -0,0 +1,16 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    int n1,n2,n3;
+    scanf("%d %d %d",&n1,&n2,&n3);
+    printf("%d\n",&n3);
+    printf("%d\n",&n2);
+    printf("%d\n",&n1);
+    printf("\n");
+    printf("%d\n",&n1);
+    printf("%d\n",&n2);
+    printf("%d\n",&n3);
+    return 0;
+}
