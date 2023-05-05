9139bc8a6b328280d1907245a544d53c0a2df62a
Yasmin <souzasmimi@gmail.com>
2023-05-05 16:30:35 -0300

    5and6

--- ex5.c
+++ ex5.c
@@ -1,3 +1,14 @@
+/*Calcule o consumo médio de um automóvel sendo fornecidos a distância total percorrida (em Km) e o total de combustível gasto (em litros).
+Entrada
+O arquivo de entrada contém dois valores: um valor inteiro X representando a distância total percorrida (em Km), e um valor real Y representando o total de combustível gasto, com um dígito após o ponto decimal.
+Saída
+Apresente o valor que representa o consumo médio do automóvel com 3 casas após a vírgula, seguido da mensagem "km/l".
+exemplo de 
+entrada   saida
+500       14.286 km/l  
+35.0
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 int main() {
@@ -6,6 +17,6 @@ int main() {
   scanf("%d",&X);
   scanf("%f",&y);
   cons = X/y;
-  printf("%2.f km/l",cons);
+  printf("%.3f km/l\n",cons);
   return 0;
 }
