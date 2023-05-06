2b62b134cc4cf14cebecea06daa92e50658369dc
Yasmin Souza <70536067+mimi124515@users.noreply.github.com>
2023-05-06 14:02:33 -0300

    Add files via upload

---
+++ 33-43/41.c
@@ -0,0 +1,21 @@
+/*Escreva um programa que repita a leitura de uma senha até que ela seja válida. Para cada leitura de senha incorreta informada, escrever a mensagem "Senha Invalida". Quando a senha for informada corretamente deve ser impressa a mensagem "Acesso Permitido" e o algoritmo encerrado. Considere que a senha correta é o valor 2002. 
+
+Entrada
+A entrada é composta por vários casos de testes contendo valores inteiros.
+
+Saída
+Para cada valor lido mostre a mensagem correspondente à descrição do problema.*/
+#include <stdio.h>
+int main(){
+    int senha;
+    while (1){
+        scanf("%d", &senha);
+        if (senha == 2002){
+            printf("Acesso Permitido\n");
+            break;
+        } else {
+            printf("Senha Invalida\n");
+        }
+    }
+    return 0;
+}
