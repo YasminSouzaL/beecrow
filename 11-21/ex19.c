c353b08a9c709b0231afcd4e4249a2bfd26790cf
Yasmin <souzasmimi@gmail.com>
2023-05-05 22:51:05 -0300

    path

---
+++ 11-21/ex19.c
@@ -0,0 +1,22 @@
+/*Leia a hora inicial, minuto inicial, hora final e minuto final de um jogo. A seguir calcule a duração do jogo.
+
+Obs: O jogo tem duração mínima de um (1) minuto e duração máxima de 24 horas.
+
+Entrada
+Quatro números inteiros representando a hora de início e fim do jogo.
+
+Saída
+Mostre a seguinte mensagem: “O JOGO DUROU XXX HORA(S) E YYY MINUTO(S)” 
+
+*/
+#include <stdio.h>
+int main()
+{
+    int a, b, c, d;
+    int dif;
+    scanf("%d %d %d %d", &a, &c, &b, &d);
+    dif = ((b*60)+d) - ((a*60)+c);
+    if(dif<=0) dif += 24*60;
+    printf("O JOGO DUROU %d HORA(S) E %d MINUTO(S)\n", dif/60, dif%60);
+    return 0;
+}
